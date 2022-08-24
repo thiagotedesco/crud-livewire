650ae0fa742fa2267dbd0df7ece2873062f72083
Thiago Tedesco <thiago.tedesco@crptecnologia.com.br>
2022-08-24 13:23:18 -0300

    Criação do componento Contact.php

--- crud-livewire/app/Http/Livewire/Contact.php
+++ crud-livewire/app/Http/Livewire/Contact.php
@@ -3,11 +3,81 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Contact as ContactModel;
 
 class Contact extends Component
 {
+    public $contactId;
+
+    public $name;
+    public $email;
+    public $phone;
+
+    protected $rules = [
+        'name'  => 'required|min:3',
+        'email' => 'required|min:3',
+        'phone' => 'required|min:3|numeric',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function render()
     {
-        return view('livewire.contact');
+        $contacts = ContactModel::paginate(10);
+        return view('livewire.contact', ['contacts' => $contacts]);
+    }
+
+    public function save()
+    {
+        if (isset($this->contactId)) {
+            $this->edit($this->contactId);
+        } else {
+            $this->create();
+        }
+    }
+
+    public function edit($contact)
+    {
+        $contact = ContactModel::find($contact);
+        $contact->update([
+            'name'  => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone
+        ]);
+
+        $this->name = $this->email = $this->phone = $this->contactId = $contact->null;
+
+        session()->flash('message', 'Contact edited');
+    }
+
+    public function create()
+    {
+        $this->validate();
+
+        ContactModel::create([
+            'name'  => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+        ]);
+
+        $this->name = $this->email = $this->phone = null;
+
+        session()->flash('message', 'Contact created');
+    }
+
+    public function delete(ContactModel $contact)
+    {
+        $contact->delete();
+    }
+
+    public function setValues(ContactModel $contact)
+    {
+        $this->contactId = $contact->id;
+        $this->name      = $contact->name;
+        $this->email     = $contact->email;
+        $this->phone     = $contact->phone;
     }
 }
--- crud-livewire/resources/views/livewire/contact.blade.php
+++ crud-livewire/resources/views/livewire/contact.blade.php
@@ -1,3 +1,78 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="container">
+    <h2 class="pt-5">Create a contact</h2>
+    @if(session()->has('message'))
+        <div class="alert alert-success">
+            {{session()->get('message')}}
+        </div>
+    @endif
+    <div>
+        <form class="" method="post" wire:submit.prevent="save">
+            <div class="form-group">
+                <label for="name" class="col">Nome</label>
+                <input class="form-control  @if($errors->has('name')) is-invalid @endif" type="text" name="nome"
+                       wire:model="name">
+                <div class="text-danger">
+                    @error('name')
+                    {{$message}}
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control @if($errors->has('email')) is-invalid @endif" type="email" name="email"
+                       wire:model="email">
+                <div class="text-danger">
+                    @error('email')
+                    {{$message}}
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" name="phone" class="form-control @if($errors->has('phone')) is-invalid @endif"
+                       wire:model="phone">
+                <div class="text-danger">
+                    @error('phone')
+                    {{$message}}
+                    @enderror
+                </div>
+            </div>
+            <br>
+            <div class="float-right">
+                <button class="btn btn-primary float-end" type="submit">
+                    Save Contact
+                </button>
+            </div>
+        </form>
+    </div>
+
+    <br><br><br>
+    <hr>
+
+    <table class="table table-striped table-hover">
+        <theader>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Action</th>
+        </theader>
+        <tbody>
+        @foreach($contacts as $contact)
+            <tr>
+                <td>{{$contact->name}}</td>
+                <td>{{$contact->email}}</td>
+                <td>{{$contact->phone}}</td>
+                <td>
+                    <a href="#" wire:click.prevent="setValues({{ $contact }})"> <i class="fa-solid fa-pen"></i></a>
+                    <a href="#" wire:click.prevent="delete({{ $contact }})"> <i class="fa-solid fa-trash-can"></i></a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $contacts->links() }}
 </div>
+
+
