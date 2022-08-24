e05b78d40836c6a812ade11de84044bebd3d4ea8
Thiago Tedesco <thiago.tedesco@crptecnologia.com.br>
2022-08-24 13:16:09 -0300

    Configurado a migration

--- crud-livewire/database/migrations/2022_08_24_160419_create_contacts_table.php
+++ crud-livewire/database/migrations/2022_08_24_160419_create_contacts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
             $table->timestamps();
         });
     }
